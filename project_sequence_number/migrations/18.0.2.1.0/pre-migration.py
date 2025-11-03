6918cb67a0f171a1475ad8f6fb9663a976a0011b
Henrik Norlin <henrik.norl@gmail.com>
2025-11-03 22:03:54 +0100

    project_sequence_number: run if version < 18.0.2.0.1

--- project_sequence_number/migrations/18.0.2.1.0/pre-migration.py
+++ project_sequence_number/migrations/18.0.2.1.0/pre-migration.py
@@ -1,12 +1,12 @@
 def migrate(cr, version):
-    cr.execute("""
-UPDATE project_project
-SET sequence_sequence = sequence_code;
+    if version < "18.0.2.0.1":
+        cr.execute("""
+            UPDATE project_project
+            SET sequence_sequence = sequence_code;
 
-UPDATE project_project
-SET sequence_code = sequence_number;
+            UPDATE project_project
+            SET sequence_code = sequence_number;
 
-UPDATE project_task
-SET code = sequence_number;
-
-    """)
+            UPDATE project_task
+            SET code = sequence_number;
+        """)
