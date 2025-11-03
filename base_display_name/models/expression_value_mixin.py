7304ea4d664ca27cceb78eb460764b2232e2feef
Henrik Norlin <henrik.norl@gmail.com>
2025-11-03 13:35:35 +0100

    base_display_name: expression.value.mixin fix

--- base_display_name/models/expression_value_mixin.py
+++ base_display_name/models/expression_value_mixin.py
@@ -8,29 +8,22 @@ _logger = logging.getLogger(__name__)
 
 
 class ExpressionValueMixin(models.AbstractModel):
-    _name = "display.name.mixin"
-    _description = "display.name.mixin"
     _inherit = "expression.value.mixin"
 
     @api.model
     def _search_display_name(self, operator, value):
+        # self._rec_names_search is readonly, so we need to work with search_fnames.
         search_fnames = self.get_field_paths_from_source("ir.model", "display_name_expression")
-        if search_fnames:
-            self._rec_names_search = search_fnames
-        return super()._search_display_name(operator, value)
+        if not search_fnames:
+            return super()._search_display_name(operator, value)
 
-        # search_fnames = self.get_field_paths_from_source("ir.model", "display_name_expression")
-        # if not search_fnames:
-        #     return super()._search_display_name(operator, value)
-
-        # # Copied from ORM models.py
-        # if operator.endswith('like') and not value and '=' not in operator:
-        #     # optimize out the default criterion of ``like ''`` that matches everything
-        #     # return all when operator is positive
-        #     return expression.FALSE_DOMAIN if operator in expression.NEGATIVE_TERM_OPERATORS else expression.TRUE_DOMAIN
-        # aggregator = expression.AND if operator in expression.NEGATIVE_TERM_OPERATORS else expression.OR
-        # return aggregator([[(field_name, operator, value)] for field_name in search_fnames])
-    
+        # Copied from ORM models.py
+        if operator.endswith('like') and not value and '=' not in operator:
+            # optimize out the default criterion of ``like ''`` that matches everything
+            # return all when operator is positive
+            return expression.FALSE_DOMAIN if operator in expression.NEGATIVE_TERM_OPERATORS else expression.TRUE_DOMAIN
+        aggregator = expression.AND if operator in expression.NEGATIVE_TERM_OPERATORS else expression.OR
+        return aggregator([[(field_name, operator, value)] for field_name in search_fnames])
 
     @api.depends(lambda self: self.get_field_paths_from_source("ir.model", "display_name_expression"))
     def _compute_display_name(self):
--- partner_sequence_number/models/res_partner.py
+++ partner_sequence_number/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 
 class ResPartner(models.Model):
     _name = "res.partner"
-    _inherit = ["res.partner", "sequence.number.mixin", "display.name.mixin"]
+    _inherit = ["res.partner", "sequence.number.mixin", "expression.value.mixin"]
     _sequence_field = "sequence_number"
     _ir_sequence_code = "res.partner"
     _sql_constraints = [
@@ -19,51 +19,3 @@ class ResPartner(models.Model):
         copy=False,
         readonly=True,
     )
-
-    # @api.depends("name")
-    # def _compute_display_name(self):
-    #     """
-    #     If display_name is stored, restart Odoo after changing display_name_expression.
-    #     """
-    #     expression = self.get_expression_from_source("ir.model", "display_name_expression")
-
-    #     if expression:
-    #         fallback_records = self.browse([])  # empty recordset
-    #         for rec in self:
-    #             display_name = rec.get_value_from_expression(expression)
-    #             if display_name:
-    #                 rec.display_name = display_name
-    #             else:
-    #                 fallback_records |= rec  # collect records for super
-
-    #         if fallback_records:
-    #             super(type(self), fallback_records)._compute_display_name()
-    #     else:
-    #         super()._compute_display_name()
-
-    # @api.depends("name")
-    # def _compute_display_name(self):
-    #     expression = self.get_expression_from_source("ir.model", "display_name_expression")
-
-    #     if not expression:
-    #         # fallback: call super on the whole recordset
-    #         return super()._compute_display_name()
-
-    #     # records that can be computed via expression
-    #     expr_records = self.browse([])
-    #     fallback_records = self.browse([])
-
-    #     for rec in self:
-    #         display_name = rec.get_value_from_expression(expression)
-    #         if display_name:
-    #             rec.display_name = display_name
-    #             expr_records |= rec
-    #         else:
-    #             fallback_records |= rec
-
-    #     # call super ONLY on fallback_records
-    #     if fallback_records:
-    #         # use the base class explicitly, not self.__class__
-    #         super(type(self), fallback_records)._compute_display_name()
-
-
