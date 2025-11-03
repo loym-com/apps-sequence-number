2589fc1129f135c119d5cb43190f48ca39406bf3
Henrik Norlin <henrik.norl@gmail.com>
2025-11-03 21:40:47 +0100

    project_sequence_number: disabled unique sequence_code

--- project_sequence_number/models/project_project.py
+++ project_sequence_number/models/project_project.py
@@ -6,13 +6,13 @@ class ProjectProject(models.Model):
     _inherit = ["project.project", "sequence.number.mixin", "expression.value.mixin"]
     _sequence_field = "sequence_code" # Same as in OCA project_sequence
     _ir_sequence_code = "project.sequence"
-    _sql_constraints = [
-        (
-            "unique_sequence",
-            "UNIQUE(sequence_code)",
-            "sequence_code must be unique!",
-        ),
-    ]
+    # _sql_constraints = [
+    #     (
+    #         "unique_sequence",
+    #         "UNIQUE(sequence_code)",
+    #         "sequence_code must be unique!",
+    #     ),
+    # ]
 
     sequence_code = fields.Char(
         string="No.",
