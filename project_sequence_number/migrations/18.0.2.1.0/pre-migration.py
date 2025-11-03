aae2e48b246fd0fa990940fcf22c66571fd79e08
Henrik Norlin <henrik.norl@gmail.com>
2025-11-03 22:16:59 +0100

    project_sequence_code: migration will add missing columns

--- project_sequence_number/migrations/18.0.2.1.0/pre-migration.py
+++ project_sequence_number/migrations/18.0.2.1.0/pre-migration.py
@@ -1,11 +1,16 @@
 def migrate(cr, version):
     if version < "18.0.2.0.1":
         cr.execute("""
-            UPDATE project_project
-            SET sequence_sequence = sequence_code;
+            ALTER TABLE project_project
+            ADD COLUMN IF NOT EXISTS sequence_sequence varchar,
+            ADD COLUMN IF NOT EXISTS sequence_code varchar;
+
+            ALTER TABLE project_task
+            ADD COLUMN IF NOT EXISTS code varchar;
 
             UPDATE project_project
-            SET sequence_code = sequence_number;
+            SET sequence_sequence = sequence_code,
+                sequence_code = sequence_number;
 
             UPDATE project_task
             SET code = sequence_number;
