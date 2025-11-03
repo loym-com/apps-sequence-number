b46cb9b021fa1a6806f97681d6d06b3378d4630c
Henrik Norlin <henrik.norl@gmail.com>
2025-11-03 20:35:15 +0100

    base_mixin_display_name DUMMY

---
+++ base_mixin_display_name/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Loym
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Display Name DUMMY",
+    "author": "FIQ, Loym",
+    "data": [
+    ],
+    "depends": [
+        "base",
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.1",
+    "website": "https://www.loym.com",
+}
