58fc2e5921ecb60682a9f3570fdd719e251e09c7
Henrik Norlin <henrik.norl@gmail.com>
2025-11-04 21:51:25 +0100

    pre-migration for base_display_name & base_mixin_sequence_number

--- base_mixin_sequence_number/migrations/19.0.5.0.0/pre-migration.py
+++ base_mixin_sequence_number/migrations/19.0.5.0.0/pre-migration.py
@@ -4,14 +4,50 @@ def migrate(cr, version):
             ALTER TABLE project_project
             ADD COLUMN IF NOT EXISTS sequence_sequence varchar,
             ADD COLUMN IF NOT EXISTS sequence_code varchar;
+                   
+            UPDATE project_project
+            SET sequence_sequence = sequence_code,
+                   sequence_code = unique_code
+            WHERE unique_code IS NOT NULL;
+
 
             ALTER TABLE project_task
             ADD COLUMN IF NOT EXISTS code varchar;
 
-            UPDATE project_project
-            SET sequence_sequence = sequence_code,
-                sequence_code = sequence_number;
-
             UPDATE project_task
-            SET code = sequence_number;
+            SET code = unique_code
+            WHERE unique_code IS NOT NULL;
+
+                   
+            ALTER TABLE res_partner
+            ADD COLUMN IF NOT EXISTS sequence_number varchar;
+                   
+            UPDATE res_partner
+            SET sequence_number = unique_code
+            WHERE unique_code IS NOT NULL;
+
+
+            ALTER TABLE crm_lead
+            ADD COLUMN IF NOT EXISTS sequence_number varchar;
+
+            UPDATE crm_lead
+            SET sequence_number = unique_code
+            WHERE unique_code IS NOT NULL;
+
+
+            ALTER TABLE product_template
+            ADD COLUMN IF NOT EXISTS sequence_number varchar;
+
+            UPDATE product_template
+            SET sequence_number = unique_code
+            WHERE unique_code IS NOT NULL;
+
+
+            ALTER TABLE product_product
+            ADD COLUMN IF NOT EXISTS sequence_number varchar;
+
+            UPDATE product_product
+            SET sequence_number = unique_code
+            WHERE unique_code IS NOT NULL;
+
         """)
