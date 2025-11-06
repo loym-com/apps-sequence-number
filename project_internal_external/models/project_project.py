2b59cbd9988d71baaad4222bc9c41dea1637107e
Henrik Norlin <henrik.norl@gmail.com>
2025-11-06 14:27:54 +0100

    project_internal_external: set_sequence_code fix: no loop

--- project_internal_external/models/project_project.py
+++ project_internal_external/models/project_project.py
@@ -19,14 +19,21 @@ class ProjectProject(models.Model):
         help="Value from ir.sequence"
     )
 
-    @api.constrains("company_id", "internal_external")
+    @api.constrains("company_id", "internal_external", "sequence_code")
     def set_sequence_code(self):
+        if self.env.context.get("skip_sequence_constrains"):
+            return
+
         for rec in self:
+            values = {}
             if not rec.sequence_sequence:
-                rec.sequence_sequence = self.env["ir.sequence"].next_by_code("project.sequence")
-            rec.sequence_code = rec.get_value_from_source(
-                "ir.config_parameter", "project_internal_external.project_sequence_pattern"
+                values["sequence_sequence"] = self.env["ir.sequence"].next_by_code("project.sequence")
+            values["sequence_code"] = rec.get_value_from_source(
+                "ir.config_parameter",
+                "project_internal_external.project_sequence_pattern",
             )
+            if values:
+                rec.with_context(skip_sequence_constrains=True).write(values)
 
     def write(self, vals):
         vals = self.ondelete_sequence_code_delete_also_sequence_sequence(vals)
