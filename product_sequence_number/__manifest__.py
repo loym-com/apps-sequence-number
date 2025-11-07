6afa8fb385cfafaba77333e3d5ecf7c32b251a12
Henrik Norlin <henrik.norl@gmail.com>
2025-11-07 11:57:15 +0100

    product_sequence_number: 18.0.2.0.3

--- product_sequence_number/__manifest__.py
+++ product_sequence_number/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Product No.",
     "summary": " ",
-    "version": "18.0.2.0.2",
+    "version": "18.0.2.0.3",
     "author": "FIQ, Loym",
     "website": "https://www.loym.com",
     "license": "AGPL-3",
