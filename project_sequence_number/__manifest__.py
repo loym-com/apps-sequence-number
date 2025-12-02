5cae44745867ca95939273d90e06096adeb44828
Henrik Norlin <henrik.norl@gmail.com>
2025-12-02 15:49:48 +0100

    project_sequence_number: 18.0.2.1.3

--- project_sequence_number/__manifest__.py
+++ project_sequence_number/__manifest__.py
@@ -20,6 +20,6 @@
     "license": "AGPL-3",
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "pre_uninstall_hook",
-    "version": "18.0.2.1.2",
+    "version": "18.0.2.1.3",
     "website": "https://www.loym.com",
 }
