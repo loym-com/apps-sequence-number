0140e07c0de5f6f92939c2bc5ad90394b6e2031e
Henrik Norlin <henrik.norl@gmail.com>
2025-12-03 19:08:49 +0100

    project_internal_external 19.0.5.0.4

--- project_internal_external/__manifest__.py
+++ project_internal_external/__manifest__.py
@@ -14,7 +14,7 @@
     ],
     "excludes": [""],
     "license": "LGPL-3",
-    "version": "19.0.5.0.3",
+    "version": "19.0.5.0.4",
     "website": "https://www.loym.com",
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "pre_uninstall_hook",
