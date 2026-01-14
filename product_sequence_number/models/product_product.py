f135f4130dd4b7da0aafae2835366fd823ed52e3
Henrik Norlin <henrik@loym.com>
2026-01-14 12:56:22 +0100

    product_sequence_number: unable to add constraint to product.product

--- product_sequence_number/models/product_product.py
+++ product_sequence_number/models/product_product.py
@@ -7,10 +7,11 @@ class ProductProduct(models.Model):
     _sequence_field = "sequence_number"
     _ir_sequence_code = "product.product"
 
-    _unique_sequence = models.Constraint(
-        "UNIQUE(sequence_number, company_id)",
-        "sequence_number must be unique per company!",
-    )
+    # _unique_sequence = models.Constraint(
+    #     "UNIQUE(sequence_number, company_id)",
+    #     "sequence_number must be unique per company!",
+    # )
+    # unable to add constraint 'product_product_unique_sequence_number_per_company' as UNIQUE(sequence_number, company_id)
 
     sequence_number = fields.Char(
         string="No.",
